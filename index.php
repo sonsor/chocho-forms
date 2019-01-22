174907b2e4d3bba3d0071184752652e626b9abf8
wasif <wasif@localhost.localdomain>
2019-01-22 21:43:57 +0500

    create index file for plugin

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Plugin Name: Chocho Forms
+ * Author: Wasif Farooq
+ * Description: This plugin enable yout to create you custom forms and collect data frm it.
+ * Version: 0.0.1
+ */
+
